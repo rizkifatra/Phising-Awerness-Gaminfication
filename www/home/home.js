fb70cb26e3f1f02129f3cff7f073d2bf81ee826b
Rizki Fatra <abdulhadiy@Abduls-MacBook-Pro-81.local>
2025-01-06 15:26:46 +0800

    second

--- www/home/home.js
+++ www/home/home.js
@@ -4,11 +4,11 @@ document.getElementById("playGameButton").addEventListener("click", () => {
 });
 
 document.getElementById("leaderboardButton").addEventListener("click", () => {
-  window.location.href = "leaderboard.html";
+  window.location.href = "../leaderboard/leaderboard.html";
 });
 
-document.getElementById("viewProgressButton").addEventListener("click", () => {
-  window.location.href = "view_progress.html";
+document.getElementById("profileButton").addEventListener("click", () => {
+  window.location.href = "../profile/profile.html";
 });
 
 document
@@ -16,3 +16,7 @@ document
   .addEventListener("click", () => {
     window.location.href = "view_performance.html";
   });
+
+document.getElementById("playGameButton").addEventListener("click", () => {
+  window.location.href = "../play/play.html";
+});
