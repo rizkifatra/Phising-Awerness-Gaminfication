3dc00bdfd2383ef297b1e88111ace7e86eaaf1c7
Rizki Fatra <abdulhadiy@Abduls-MacBook-Pro-120.local>
2025-01-07 17:42:24 +0800

    third

---
+++ www/profile/profile.js
@@ -0,0 +1,4 @@
+// Redirect to the respective pages when buttons are clicked
+document.getElementById("manageButton").addEventListener("click", () => {
+  window.location.href = "manage/manage.html";
+});
